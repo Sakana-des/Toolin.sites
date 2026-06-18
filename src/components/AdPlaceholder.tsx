b296b4b005f7b4a5774c429f396f0e5be22168c2
Atha Khoirun Ayyasi <iyas123yas@gmail.com>
2026-06-18 18:24:42 +0700

    feat: add AdPlaceholder component to support Google AdSense and Adsterra ad integrations

--- src/components/AdPlaceholder.tsx
+++ src/components/AdPlaceholder.tsx
@@ -55,7 +55,7 @@ export default function AdPlaceholder({
   const [isClient, setIsClient] = useState(false);
   
   const adsenseClient = process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID;
-  const adsterraEnabled = process.env.NEXT_PUBLIC_ADSTERRA_ENABLED === "true";
+  const adsterraEnabled = process.env.NEXT_PUBLIC_ADSTERRA_ENABLED !== "false";
 
   useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
